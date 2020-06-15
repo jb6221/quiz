4afae3a193cfc1f2876619dba79d05f48d6dbd6a
Milk-Star <jonathanrbowen@gmail.com>
2020-06-15 14:37:50 -0400

    Updates

---
+++ precache-manifest.675d484a5b6311709657f9233dfdcb3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de733427492d61df8110ead7c0e397ac",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "c6fce5be83b390912d00",
+    "url": "/quiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9c05c29f4a158c1fb047",
+    "url": "/quiz/static/js/2.cddd331e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz/static/js/2.cddd331e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6fce5be83b390912d00",
+    "url": "/quiz/static/js/main.05f6cf86.chunk.js"
+  },
+  {
+    "revision": "f3a90f501f89ae00aea3",
+    "url": "/quiz/static/js/runtime-main.95e2c44f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
